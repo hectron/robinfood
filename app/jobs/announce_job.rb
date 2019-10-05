69c295688e243efcac622fc68619bb1508b45552
Hector Rios <that.hector@gmail.com>
2019-10-05 16:01:30 -0500

    Add announcer job

--- app/jobs/announce_job.rb
+++ app/jobs/announce_job.rb
@@ -2,6 +2,7 @@ class AnnounceJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    pass
+    decision = Recommendations::Engines::V2.new(Date.current).generate
+    SlackAdapter.announce(decision, channel: '#app-robinhood')
   end
 end
