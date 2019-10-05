eeba39f7d906ddd07fe5e5a8039dbb990ed07e1a
Hector Rios <that.hector@gmail.com>
2019-10-05 17:15:24 -0500

    Post for that day

--- app/jobs/announce_job.rb
+++ app/jobs/announce_job.rb
@@ -7,7 +7,7 @@ class AnnounceJob < ActiveJob::Base
   def perform(args)
     channel  = args.fetch('channel')
     budget   = args.fetch('budget').to_f
-    decision = Recommendations::Engine::V2.new(3.days.from_now, budget).generate
+    decision = Recommendations::Engine::V2.new(Date.today, budget).generate
 
     SlackAdapter.announce(decision, channel: channel)
   end
