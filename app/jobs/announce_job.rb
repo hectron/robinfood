d7c0001c2c57236adf0ea2d442c35450e4c764c7
Hector Rios <that.hector@gmail.com>
2020-01-11 16:59:57 -0600

    Add Vegetarian Engine to the morning message

--- app/jobs/announce_job.rb
+++ app/jobs/announce_job.rb
@@ -2,10 +2,12 @@ class AnnounceJob < ActiveJob::Base
   queue_as :default
 
   def perform(args)
-    channel  = args.fetch('channel')
-    budget   = args.fetch('budget').to_f
-    decision = Recommendations::Engines::V2.new(Date.today, budget).generate
+    channel             = args.fetch('channel')
+    budget              = args.fetch('budget').to_f
+    decision            = Recommendations::Engine::V2.new(Date.today, budget).generate
+    vegetarian_decision = Recommendations::Engine::V3.new(Date.today, budget).generate
 
     SlackAdapter.announce(decision, channel: channel)
+    SlackAdapter.announce(vegetarian_decision, channel: channel)
   end
 end
