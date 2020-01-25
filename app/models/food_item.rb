d71d0d96afce1f3118ae25bcd8f33f5f650f2f5c
Hector Rios <hector.rios@amount.com>
2020-01-25 14:24:39 -0600

    Rubocop fixes

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 class FoodItem < ApplicationRecord
   scope :main_dishes, -> { where(category: MAIN_DISHES) }
   scope :side_dishes, -> { where(category: SIDE_DISHES) }
-  scope :below, -> (budget) { where('price <= ?', budget) }
-  scope :offered, -> (date) { where(date_offered: date) }
+  scope :below, ->(budget) { where("price <= ?", budget) }
+  scope :offered, ->(date) { where(date_offered: date) }
 
-  ENTREE   = 'Entrees'.freeze
-  SALAD    = 'Salads'.freeze
-  SOUP     = 'Soups'.freeze
-  SIDE     = 'Sides'.freeze
-  BEVERAGE = 'Beverages'.freeze
-  DESSERT  = 'Desserts'.freeze
+  ENTREE = "Entrees"
+  SALAD = "Salads"
+  SOUP = "Soups"
+  SIDE = "Sides"
+  BEVERAGE = "Beverages"
+  DESSERT = "Desserts"
 
-  MAIN_DISHES = [ENTREE, SALAD, SOUP]
-  SIDE_DISHES = [SIDE, BEVERAGE, DESSERT]
+  MAIN_DISHES = [ENTREE, SALAD, SOUP].freeze
+  SIDE_DISHES = [SIDE, BEVERAGE, DESSERT].freeze
 
   def self.from_element(element, date_offered)
-    restaurant           = element.attribute('data-vendor_name').gsub(/\W+$/, '')
-    category             = element.attribute('data-category')
-    name                 = element.at_css('.item__name').text.squish
-    price                = element.at_css('.item__price').text.gsub!('$', '').to_f
-    url                  = element.at_css('a').attribute('href')
-    dietary_restrictions = (element.attribute('data-dietary_restriction') || '').split(',')
+    restaurant = element.attribute("data-vendor_name").gsub(/\W+$/, "")
+    category = element.attribute("data-category")
+    name = element.at_css(".item__name").text.squish
+    price = element.at_css(".item__price").text.delete!("$").to_f
+    url = element.at_css("a").attribute("href")
+    dietary_restrictions = (element.attribute("data-dietary_restriction") || "").split(",")
 
     new.tap do |instance|
-      instance.name                 = name
-      instance.price                = price
-      instance.restaurant           = restaurant
-      instance.category             = category
-      instance.url                  = url
-      instance.date_offered         = date_offered
+      instance.name = name
+      instance.price = price
+      instance.restaurant = restaurant
+      instance.category = category
+      instance.url = url
+      instance.date_offered = date_offered
       instance.dietary_restrictions = dietary_restrictions
     end
   end
@@ -54,7 +56,7 @@ class FoodItem < ApplicationRecord
   def last_different_priced_item
     FoodItem
       .where(name: name, restaurant: restaurant)
-      .where('price <> ? AND date_offered < ?', price, date_offered)
+      .where("price <> ? AND date_offered < ?", price, date_offered)
       .order(date_offered: :desc)
       .first
   end
